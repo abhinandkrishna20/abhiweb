c80fa96101621f5d627d26b851476a801504f2f0
Abhinand Krishna A <am.abhinandkrishna@gmail.com>
2025-05-23 01:15:07 +0530

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 
 
+import Footer from './components/Footer'
 import Header from './components/Header'
 
 function App() {
 
 
   return (
-    <div className='bg-white dark:bg-gray-800 w-full h-full  items-center justify-center'>
+    <div className='bg-white dark:bg-gray-800 w-full min-h-screen  flex flex-col'>
      <Header />
+     <main className="flex-grow m-3 text-white">
+      
+          
+        this ithe main content
+      </main>
+     <Footer />
   
     </div>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='border-t-2 border-blue-50 w-full p-3  text-blue-50 '>
+      <div className='items-baseline-last justify-center'>
+        All rights reserver @ Abhinand Krishna
+      </div>
+      <div className='flex items-center justify-center-safe'>
+        <div className='text-blue-50 hover:text-green-400 px-3'><a href=""> Facebook </a></div>
+        
+        <div className='text-blue-50 hover:text-green-400  px-3'><a href=""> Twitter </a></div>
+        
+        <div className='text-blue-50 hover:text-green-400  px-3'><a href=""> Instagram </a></div>
+
+      </div>
+
+    </div>
   )
 }
 
